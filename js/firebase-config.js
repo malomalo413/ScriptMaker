f32852d6b4d8ad376af407955d714642fd6b67ca
malomalo413 <malo.imo413@gmail.com>
2026-07-04 13:34:22 +0900

    Configure Firebase project

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,9 @@
 window.SCRIPTMAKER_FIREBASE_CONFIG = window.SCRIPTMAKER_FIREBASE_CONFIG || {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyAh1wyh8zH7SdkflfkHnJOndtpsHEzxpRE",
+  authDomain: "small-4c16f.firebaseapp.com",
+  projectId: "small-4c16f",
+  storageBucket: "small-4c16f.firebasestorage.app",
+  messagingSenderId: "737410895245",
+  appId: "1:737410895245:web:4bfc221144fa9e81fd81c4",
+  measurementId: "G-E15RE7TYXL"
 };
